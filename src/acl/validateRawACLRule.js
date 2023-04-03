daa306428e6a4c9cb06e45cd7895d59c85525237
anio.software <git@anio.software>
2023-04-03 11:09:32 +0200

    Add validateRawACLRule.

---
+++ src/acl/validateRawACLRule.js
@@ -0,0 +1,58 @@
+const arrayify = require("../util/arrayify.js")
+const parseACLTargetNotation = require("../util/parseACLTargetNotation.js")
+const {mapProtocolNameToInteger} = require("../util/map/protocol.js")
+
+module.exports = function(rule) {
+	const source = parseACLTargetNotation(rule.source)
+	const destination = parseACLTargetNotation(rule.destination)
+
+	const source_ports = arrayify(source.ports)
+	const destination_ports = arrayify(destination.ports)
+
+	if (source_ports.length > 1) {
+		throw new Error(
+			`Cannot have more than one source port (got ${source_ports.length}): '${rule.source}'.`
+		)
+	} else if (destination_ports.length > 1) {
+		throw new Error(
+			`Cannot have more than one destination port (got ${destination_ports.length}): '${rule.destination}'.`
+		)
+	}
+
+	const source_port_set = source_ports[0] !== "any"
+	const destination_port_set = destination_ports[0] !== "any"
+
+	let protocol_no = null
+
+	const protocols_with_ports = [6, 17] /* tcp, udp */
+
+	if ("protocol" in rule) {
+		protocol_no = mapProtocolNameToInteger(rule.protocol)
+	}
+
+	if (source_port_set) {
+		if (protocol_no === null) {
+			throw new Error(
+				`Cannot have a source port (${source_ports[0]}) without matching protocol (protocol given: none).`
+			)
+		} else if (!protocols_with_ports.includes(protocol_no)) {
+			throw new Error(
+				`Cannot have a source port (${source_ports[0]}) without matching protocol (protocol given: ${rule.protocol}).`
+			)
+		}
+	} else if (destination_port_set) {
+		if (protocol_no === null) {
+			throw new Error(
+				`Cannot have a destination port (${destination_ports[0]}) without matching protocol (protocol given: none).`
+			)
+		} else if (!protocols_with_ports.includes(protocol_no)) {
+			throw new Error(
+				`Cannot have a destination port (${destination_ports[0]}) without matching protocol (protocol given: ${rule.protocol}).`
+			)
+		}
+	}
+
+	if (!("action" in rule)) {
+		throw new Error(`action is missing.`)
+	}
+}
---
+++ test/acl/validateRawACLRule.test.js
@@ -0,0 +1,87 @@
+const validateRawACLRule = require("../../src/acl/validateRawACLRule.js")
+const assert = require("assert")
+
+describe("acl:validateRawACLRule", () => {
+
+	it("should throw an error if multiple source ports are specified", () => {
+		assert.throws(() => {
+			validateRawACLRule({
+				source: "10.0.0.1:24,25",
+				destination: "any",
+				protocol: "udp"
+			})
+		}, {
+			message: "Cannot have more than one source port (got 2): '10.0.0.1:24,25'."
+		})
+	})
+
+	it("should throw an error if multiple destination ports are specified", () => {
+		assert.throws(() => {
+			validateRawACLRule({
+				source: "any",
+				destination: "10.0.0.1:24,25",
+				protocol: "udp"
+			})
+		}, {
+			message: "Cannot have more than one destination port (got 2): '10.0.0.1:24,25'."
+		})
+	})
+
+	it("should throw an error if source port is specified without protocol", () => {
+		assert.throws(() => {
+			validateRawACLRule({
+				source: "any:24",
+				destination: "10.0.0.1"
+			})
+		}, {
+			message: "Cannot have a source port (24) without matching protocol (protocol given: none)."
+		})
+	})
+
+	it("should throw an error if source port is specified with wrong protocol", () => {
+		assert.throws(() => {
+			validateRawACLRule({
+				source: "any:24",
+				destination: "10.0.0.1",
+				protocol: "icmp"
+			})
+		}, {
+			message: "Cannot have a source port (24) without matching protocol (protocol given: icmp)."
+		})
+	})
+
+	it("should throw an error if destination port is specified without protocol", () => {
+		assert.throws(() => {
+			validateRawACLRule({
+				source: "any",
+				destination: "10.0.0.1:24"
+			})
+		}, {
+			message: "Cannot have a destination port (24) without matching protocol (protocol given: none)."
+		})
+	})
+
+	it("should throw an error if destination port is specified wrong protocol", () => {
+		assert.throws(() => {
+			validateRawACLRule({
+				source: "any",
+				destination: "10.0.0.1:24",
+				protocol: "icmp"
+			})
+		}, {
+			message: "Cannot have a destination port (24) without matching protocol (protocol given: icmp)."
+		})
+	})
+
+	it("should throw an error if no action was specified", () => {
+		assert.throws(() => {
+			validateRawACLRule({
+				source: "any",
+				destination: "10.0.0.1",
+				protocol: "icmp"
+			})
+		}, {
+			message: "action is missing."
+		})
+	})
+})
