3d89c0a3d453487f4f17950c041c13967db82fbb
anio.software <git@anio.software>
2023-04-12 16:15:16 +0200

    Fix bug: add digits to valid location name characters.

--- src/basic/validateLocationName.js
+++ src/basic/validateLocationName.js
@@ -2,6 +2,7 @@ const getAlphabetAsArray = require("../util/getAlphabetAsArray.js")
 
 let allowed_characters = [
 	...getAlphabetAsArray(),
+	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
 	"-", "@", "_", ":", "/", ".", " "
 ]
 
