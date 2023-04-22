7e7e8c99def8a3671a6fd8bcfc54ce6274cb7731
dasdeo <dasdeo@proton.me>
2023-04-22 07:45:45 +0200

    - Add config option 'users' to specify multiple user accounts. - Release 0.0.2.

--- src/basic/convert.js
+++ src/basic/convert.js
@@ -15,7 +15,15 @@ module.exports = function(device_config) {
 	str += `no system-time dst\n`
 	str += `#\n`.repeat(5)
 	str += `mac address-table vlan-security mode drop\n`
-	str += `user name ${device_config.username} privilege admin password 0 ${device_config.password}\n`
+
+	if ("users" in device_config) {
+		for (let user of device_config.users) {
+			str += `user name ${user.username} privilege admin password 0 ${user.password}\n`
+		}
+	} else if ("username" in device_config && "password" in device_config) {
+		str += `user name ${device_config.username} privilege admin password 0 ${device_config.password}\n`
+	}
+
 	str += `telnet disable\n`
 	str += `no service reset-disable\n`
 	str += `#\n`.repeat(10)
