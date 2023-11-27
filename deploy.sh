1e3835fb03e47bead68106b387466e346cce3772
dasdeo <dasdeo@proton.me>
2023-11-27 07:46:50 +0100

    Add auto deploy on main branch.

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eufx
+
+npm i
+
+rm -f ./dist/tpcfggen.mjs
+
+./node_modules/.bin/anio_bundler .
+
+curl \
+	--request POST \
+	--data-binary "@./dist/tpcfggen.mjs" \
+	-H "Content-Type:application/octet-stream" \
+	-H "x-anio-auth-key: $ANIO_DEPLOY_KEY" \
+	-H "x-anio-file-name: tpcfggen" \
+	https://anio.sh/upload
